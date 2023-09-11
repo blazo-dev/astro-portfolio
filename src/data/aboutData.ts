8c09bd27147d34c825dbf2d42b67936d6e00843e
blazo-dev <blazo.dev@gmail.com>
2023-09-10 22:00:47 -0600

    Removing multiple pages, creating experience and about section

---
+++ src/data/aboutData.ts
@@ -0,0 +1,9 @@
+export const aboutInfo = {
+	title:
+		"Greetings, I'm Bryan Lazo! Navigating code, design and creativity to create the future of exceptional web experiences.",
+	paragraph: [
+		'My career began with a focus on Front-End development, where I honed UI creation with Angular, implemented features as designed, and collaborated closely with teams to integrate APIs and solve cross-browser compatibility issues.',
+		'Over time, expanded my skill set as a Full Stack developer, exploring Back-End technologies like Java and Oracle, and automating tasks with UC4. Also specialized in database management using PL/SQL and SQL queries.',
+		'Today, my passion for web development in general and the cloud has grown tremendously. This passion constantly drives me to improve in all aspects, have skills to develop both in Front-End and Back-End, as well as in server management. Currently, immersed in projects that span all of these areas, allowing me to explore and hone my skills across the entire spectrum of web development and cloud computing. Focused on this exciting journey of learning and evolution!'
+	]
+}
---
+++ src/data/mainData.ts
@@ -0,0 +1,6 @@
+export const mainContent = {
+	title: 'Web Developer',
+	name: 'Bryan Lazo',
+	slogan: 'Coffee, Creativity, and Code.',
+	writterText: 'Welcome to my digital world.'
+}
